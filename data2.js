ba7e8fead5b1af23667ac6e0e4a77ba48ea676dd
webstoryboy <webstupids@gmail.com>
2021-05-01 14:18:06 +0900

    Create data2.js

---
+++ data2.js
@@ -0,0 +1,20 @@
+[
+  {
+    id: 0,
+    title: "라이언 인형",
+    content: "Born in France",
+    price: 120000,
+  },
+  {
+    id: 1,
+    title: "어피치 인형",
+    content: "Born in Seoul",
+    price: 110000,
+  },
+  {
+    id: 2,
+    title: "콘 인형",
+    content: "Born in the States",
+    price: 130000,
+  },
+];
